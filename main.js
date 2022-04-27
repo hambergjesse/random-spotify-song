a6656fc4961efc2ebdf734153b6d4f12dc1f1fb4
Jesse <hambergjesse@gmail.com>
2022-04-27 13:24:08 +0300

    this shit hard.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function generateSong() {
-  let random_seed = makeid(2);
+  /*let random_seed = makeid(2);
   let random_offset = Math.floor(Math.random() * 2000); // returns a random integer from 0 to 9
-  $.ajax({
+  ({
     url:
       "https://api.spotify.com/v1/search?type=track&offset=" +
       random_offset +
@@ -9,4 +9,5 @@ function generateSong() {
       random_seed,
     type: "GET",
   });
+  */
 }
