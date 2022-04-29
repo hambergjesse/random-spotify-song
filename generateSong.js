25625714c3592d43918fa1a1ca24fcfd26fb8b9d
Jesse <hambergjesse@gmail.com>
2022-04-29 11:09:23 +0300

    stuck (idk)

--- generateSong.js
+++ generateSong.js
@@ -1,43 +1,50 @@
 function makeId(length) {
-  let songID = "";
+  let result = "";
   let characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
-  return songID;
+  return result;
 }
 
-function generateSong() {
-  // fetch random song
-  function getASong() {
-    let random_seed = makeId(2);
-    let random_offset = Math.floor(Math.random() * 2000); // returns a random integer from 0 to 9
-    let options = {
-      url:
-        "https://api.spotify.com/v1/search?type=track&offset=" +
-        random_offset +
-        "&limit=1&q=" +
-        random_seed,
-      type: "GET",
-    };
-    console.log(options);
-    return options;
-  }
-
-  //replace song ID
-  function replaceID() {
-    document
-      .querySelector(".spotify-song")
-      .attr(
-        "src",
-        "https://open.spotify.com/embed/track/" +
-          options +
-          "?utm_source=generator&theme=0"
-      );
-  }
+// fetch random song
+function getASong() {
+  let random_seed = makeId(2);
+  let random_offset = Math.floor(Math.random() * 2000); // returns a random integer from 0 to 9
+  let options = {
+    url:
+      "https://api.spotify.com/v1/search?type=track&offset=" +
+      random_offset +
+      "&limit=1&q=" +
+      random_seed,
+    type: "GET",
+    success: function (replaceID) {
+      document
+        .querySelector(".spotify-song")
+        .attr(
+          "src",
+          "https://open.spotify.com/embed/track/" +
+            options +
+            "?utm_source=generator&theme=0"
+        );
+      return replaceID;
+    },
+  };
 }
+
+//replace song ID
+/*function replaceID() {
+  document
+    .querySelector(".spotify-song")
+    .attr(
+      "src",
+      "https://open.spotify.com/embed/track/" +
+        options +
+        "?utm_source=generator&theme=0"
+    );
+}*/
 
 // fetch random song & replace ID in source URL
 /*function getASong() {
