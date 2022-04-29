26f6f3c14fa5431f372685a22b78665c0dac48bc
Jesse <hambergjesse@gmail.com>
2022-04-29 10:55:00 +0300

    new way

--- generateSong.js
+++ generateSong.js
@@ -9,8 +9,38 @@ function makeId(length) {
   return songID;
 }
 
+function generateSong() {
+  // fetch random song
+  function getASong() {
+    let random_seed = makeId(2);
+    let random_offset = Math.floor(Math.random() * 2000); // returns a random integer from 0 to 9
+    let options = {
+      url:
+        "https://api.spotify.com/v1/search?type=track&offset=" +
+        random_offset +
+        "&limit=1&q=" +
+        random_seed,
+      type: "GET",
+    };
+    console.log(options);
+    return options;
+  }
+
+  //replace song ID
+  function replaceID() {
+    document
+      .querySelector(".spotify-song")
+      .attr(
+        "src",
+        "https://open.spotify.com/embed/track/" +
+          options +
+          "?utm_source=generator&theme=0"
+      );
+  }
+}
+
 // fetch random song & replace ID in source URL
-function getASong() {
+/*function getASong() {
   let random_seed = makeId(2);
   let random_offset = Math.floor(Math.random() * 2000); // returns a random integer from 0 to 9
   let options = {
@@ -34,4 +64,4 @@ function getASong() {
     },
   };
   console.log(options);
-}
+}*/
